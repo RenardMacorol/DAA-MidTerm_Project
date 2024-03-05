c8e8ef49c9f71d91aa5618d4255b614f4e02eed5
RenardFake <rmacorol18@gmail.com>
2024-03-05 18:10:58 +0800

    add some and some

---
+++ LabelTest.java
@@ -0,0 +1,13 @@
+import javax.swing.JLabel;
+
+public class LabelTest extends Frame {
+    JLabel label ;
+
+    LabelTest(){
+        //label Settings
+        label = new JLabel("Midterm Project");
+        label.setBounds(200, 200, 200, 200);
+        frame.add(label);
+    }
+ 
+}
--- MainApp.java
+++ MainApp.java
@@ -2,19 +2,9 @@ import javax.swing.*;
 
 public class MainApp{
     public static void main(String[] args) {
-        JFrame frame = new JFrame("ERE");
-        JButton butones = new JButton("Button");
-        JButton aboutButton = new JButton("About Us They/Them wbahhaha");
-        //frame settings
-        frame.setSize(1080,800);
-        frame.setVisible(true); // sige i false moto yari ka sakin charutt
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Butones settings
-        butones.setBounds(20,20,20,20);//isipin nyo yung butones parng rectancle 4 side rectangle diba
-        aboutButton.setBounds(100,100,100,100);
-        //Add elements
-        frame.add(butones);
-        frame.add(aboutButton);
+        Frame frame = new Frame();
+        LabelTest label = new LabelTest();
+        
     }
    
     
