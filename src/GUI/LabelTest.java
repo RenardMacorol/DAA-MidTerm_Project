987e896f6a808c48e30f0191e8445a93e516861f
RenardFake <rmacorol18@gmail.com>
2024-03-09 18:02:34 +0800

    redo

---
+++ src/GUI/LabelTest.java
@@ -0,0 +1,14 @@
+package src.GUI;
+import javax.swing.JLabel;
+
+public class LabelTest extends Frame {
+    JLabel label ;
+
+    public LabelTest(){
+        //label Settings
+        label = new JLabel("Midterm Project");
+        label.setBounds(200, 200, 200, 200);
+        frame.add(label);
+    }
+ 
+}
