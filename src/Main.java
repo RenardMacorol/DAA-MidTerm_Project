6bbce5b6e342c97fda8a9dcccf19e38ef2e70d0a
RenardFake <rmacorol18@gmail.com>
2024-03-09 16:45:51 +0800

    IntelliJ GUI

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ package src;
 import javax.swing.*;
 
 public class Main extends JFrame {
+    private JLabel Welcome;
+    private JPanel MainPanel;
+    private JButton selectionSortButton;
+    private JButton addressSearchButton;
+    private JButton customerDeliveryButton;
+    private JButton knapsackButton;
+
     public Main(){
+        setContentPane(MainPanel);
         setTitle("Midterm Project");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800,800);
