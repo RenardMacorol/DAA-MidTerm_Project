25b9ecf96770661b16745b4990cabd55db5baeab
RenardFake <rmacorol18@gmail.com>
2024-03-09 16:26:39 +0800

    I code this on itellJ

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package src;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main(){
+        setTitle("Midterm Project");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,800);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
