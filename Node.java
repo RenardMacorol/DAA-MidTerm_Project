4988c04699fa0e71a568b7179fa2edbbbaf4a9a5
Renard B. Macorol <84180143+RenardMacorol@users.noreply.github.com>
2024-03-09 18:15:26 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,19 @@
+package CustomerDelivery;
+
+public class Node {
+    private String location;
+    
+    Node(String location) {
+    
+        this.location = location;
+    }
+
+    public void setData(String data) {
+        this.location = data;
+    }
+
+    public String getData() {
+        return location;
+    }
+
+}
