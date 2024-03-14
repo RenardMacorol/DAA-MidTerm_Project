1b3ab7e1df279cccd0dcd5dd501f8558b955e834
RenardFake <rmacorol18@gmail.com>
2024-03-14 11:02:35 +0800

    Main to fram 4

--- src/MainApp.java
+++ src/MainApp.java
@@ -19,45 +19,34 @@ import src.TSP.Tsp;
 public class MainApp extends JFrame implements ActionListener {
 
     public MainApp() {
+        //Di malinis code ko dine yawa basta nasa iisang panel lang sya
         JPanel startPanel = new JPanel();
-        
         startPanel.setLayout(new BorderLayout());
-        ImageIcon attitude = new ImageIcon("resources/attitude.png");
+        startPanel.setBounds(0, 0, 500, 500);
+
+        //Image 
+        ImageIcon attitudeIcon = new ImageIcon("resources/attitude.png");
+        JLabel attitudeLabel = new JLabel();
+        attitudeLabel.setIcon(attitudeIcon);
+        attitudeLabel.setPreferredSize(new Dimension(20, 20));
+        attitudeLabel.setBounds(0,500 ,500,500);
+        attitudeLabel.setVerticalAlignment(JLabel.CENTER);
+        attitudeLabel.setHorizontalAlignment(JLabel.CENTER);
         
-        JLabel attitudeIcon = new JLabel();
-        attitudeIcon.setIcon(attitude);
-        attitudeIcon.setPreferredSize(new Dimension(20, 20));
-        attitudeIcon.setBounds(0,500 ,500,500);
-        attitudeIcon.setVerticalAlignment(JLabel.CENTER);
-        attitudeIcon.setHorizontalAlignment(JLabel.CENTER);
-        
+        //Only one button only one yieee
         JButton start = new JButton("Start");
-        
         start.setBounds(425, 370, 100, 100);
         start.addActionListener(this);
+        
+        //add to the panel
         startPanel.add(start,BorderLayout.CENTER);
-        /*
-         * JButton knapSackButton = new JButton("Knapsack");
-         * knapSackButton.setBounds(100,100,100,100);
-         * JButton selectionSortButton = new JButton("Selection Sort");
-         * selectionSortButton.setBounds(200,100,100,100);
-         * JButton tspButton = new JButton("Delivery Address ");
-         * tspButton.setBounds(300,100,100,100);
-         * JButton addressSearchButton = new JButton("Search Address");
-         * addressSearchButton.setBounds(400,100,100,100);
-         * this.add(knapSackButton);
-         * this.add(selectionSortButton);
-         * this.add(tspButton);
-         * this.add(addressSearchButton);
-         */
+        startPanel.add(attitudeLabel,BorderLayout.CENTER);
         setTitle("Midterm Project");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1000, 563);
-        setBackground(Color.BLACK);
         setVisible(true);
         setResizable(false);
-        startPanel.add(attitudeIcon,BorderLayout.CENTER);
-        startPanel.setBounds(0, 0, 500, 500);
+        //add into the frame
         this.add(startPanel);
         
         
@@ -66,12 +55,12 @@ public class MainApp extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        //new MainApp();
+        new MainApp();
 
-         KnapsackMain knapsackMain = new KnapsackMain();
-         SelectionSort selectionSort = new SelectionSort();
-        Tsp tsp = new Tsp();
-         AddressSearchOption addressSearchOption = new AddressSearchOption();
+         //KnapsackMain knapsackMain = new KnapsackMain();
+         //SelectionSort selectionSort = new SelectionSort();
+        //Tsp tsp = new Tsp();
+         //AddressSearchOption addressSearchOption = new AddressSearchOption();
 
     }
 
