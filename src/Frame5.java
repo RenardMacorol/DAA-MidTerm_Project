43700a1feaba378220c60c91068f503b8b1b633a
RenardFake <rmacorol18@gmail.com>
2024-03-14 11:28:40 +0800

    frame 5 and 6 kain muna ako

--- src/Frame5.java
+++ src/Frame5.java
@@ -1,5 +1,57 @@
 package src;
 
-public class Frame5{
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Frame5 extends JFrame implements ActionListener{
+
+    Frame5(){
+        // Panel for this
+        JPanel guyPanel = new JPanel();
+        guyPanel.setLayout(new GridLayout(3,0));
+        guyPanel.setBounds(0, 0, 500, 500);
+
+        //Label in the top 
+        ImageIcon guyIcon = new ImageIcon();
+        JLabel guysIconLabel = new JLabel("Name this nigga");
+        guysIconLabel.setIcon(guyIcon);
+        guysIconLabel.setVerticalAlignment(JLabel.CENTER);
+        guysIconLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        //
+        JLabel guyInput = new JLabel("Guy");
+        guyInput.setVerticalAlignment(JLabel.CENTER);
+        guyInput.setHorizontalAlignment(JLabel.CENTER);
+
+        JButton proceed = new JButton("Proceed");
+        proceed.setBounds(425, 370, 100, 100);
+        proceed.addActionListener(this);
+
+        //import all components
+        guyPanel.add(guysIconLabel);
+        guyPanel.add(guyInput);
+        guyPanel.add(proceed);
+
+        //frame settings
+        setTitle("Midterm Project");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000, 563);
+        setVisible(true);
+        setResizable(false);
+        this.add(guyPanel);
+    }
+   
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+        Frame6 frame6 = new Frame6();
+    }
 }
--- src/Frame6.java
+++ src/Frame6.java
@@ -1,5 +1,65 @@
 package src;
 
-public class Frame6 {
-    
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Frame6 extends JFrame implements ActionListener{
+    Frame6(){
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new GridLayout(3,0));
+        searchPanel.setBounds(0, 0, 500, 500);
+
+        //label top
+        ImageIcon messageIcon = new ImageIcon();
+        JLabel topMessageLabel = new JLabel("find this guy?");
+        topMessageLabel.setIcon(messageIcon);
+        topMessageLabel.setVerticalAlignment(JLabel.CENTER);
+        topMessageLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel guyInput = new JLabel("Text field here for search");
+        guyInput.setVerticalAlignment(JLabel.CENTER);
+        guyInput.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel findsLabel = new JLabel();
+        //gawa kayo variable na may mag pass ng rowss and coll dine
+        findsLabel.setLayout(new GridLayout(2,2));
+        //user for loop instead
+        JLabel findtext1 = new JLabel("Tunog stree");
+        JLabel findtext2 = new JLabel("lima stree");
+        JButton button1 = new JButton("Pick");
+        JButton button2 = new JButton("Pick");
+        findsLabel.add(findtext1);
+        findsLabel.add(button1);
+        findsLabel.add(findtext2);
+        findsLabel.add(button2);
+
+        //import all label in panel
+        searchPanel.add(topMessageLabel);
+        searchPanel.add(guyInput);
+        searchPanel.add(findsLabel);
+
+        //frame settings
+        setTitle("Midterm Project");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000, 563);
+        setVisible(true);
+        setResizable(false);
+        this.add(searchPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+        Frame7 frame7 = new Frame7();
+    }
+
+
 }
